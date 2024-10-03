577a0939b67a9f6a97a3674cd2bc795521378df4
AlexPollock <pollocka@grinnell.edu>
2024-10-03 14:53:31 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -10,18 +10,43 @@ import java.io.PrintWriter;
  * 
  * @author Samuel A. Rebelsky
  * @author Your Name Here
- */ 
+ */
 public class StringUtils {
   // +------------------+--------------------------------------------
   // | Provided methods |
   // +------------------+
-    
+
   /**
    * Determine whether the parens match in string.
    */
   public static boolean checkMatching(String str) {
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
+    for (int i = 0; i < str.length(); i++) {
+      try {
+        char charAti = str.charAt(i);
+        if (charAti == '(' || charAti == '[' || charAti == '{' || charAti == '<') {
+          parens.push(charAti);
+        } else if (charAti == ')' || charAti == ']' || charAti == '}' || charAti == '>') {
+          if (parens.isEmpty()){
+            return false;
+          }
+          if ((charAti == ')' && parens.pop() == '(')) {
+            
+          } else if ((charAti == ']' && parens.pop() == '[')) {
+            
+          } else if ((charAti == '}' && parens.pop() == '{')) {
+            
+          } else if ((charAti == '>' && parens.pop() == '<')) {
+            
+          } else {
+            return false;
+          }
+        }
+      } catch (Exception e) {
+        System.err.println("Failed to put() String at: " + i);
+      }
+    }
+    return parens.isEmpty();
   } // checkMatching
-} // class StringUtils    
+} // class StringUtils
 
